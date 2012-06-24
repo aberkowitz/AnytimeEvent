12a216d306dd94ba97ad9aee62c2b8a53de0d20f
Andrew Berkowitz <a@berkowitz.org>
2012-06-23 23:41:13 -0400

    Now with more database queries

--- anytimeevent/views.py
+++ anytimeevent/views.py
@@ -5,13 +5,16 @@ from django.template import RequestContext
 from django.core import urlresolvers
 from django.contrib.auth.decorators import login_required
 
+from anytimeevent.models import EventDetails
+
 def home(request):
     return render_to_response("home.html",
                               RequestContext(request))
 
 @login_required
 def video(request, number):
-   return render_to_response("video.html",
-                              {'number': number},
+    eventDetails = EventDetails.objects.filter(id=number)
+
+    return render_to_response("video.html",
+                              {'number': number, 'eventDetails': eventDetails},
                               context_instance=RequestContext(request))
- 
