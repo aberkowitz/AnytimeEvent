a79eb7b60875c67f1565cad664b268ba44846c52
Nate Aune <natea@jazkarta.com>
2012-06-23 21:01:24 -0400

    add the necessary files to deploy to appsembler

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+# Auto-generated file
+
+import os
+import sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'appsembler_settings'
+
+import django.core.handlers.wsgi as w
+application = w.WSGIHandler()
